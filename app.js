c7f72eb4cb15dec73bbbfd70f0460a6790b937d2
shahbaz4783 <shahbaz4783@gmail.com>
2023-12-07 14:02:31 +0530

    scss initial files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ connectDB();
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(indexRoutes);
 app.use(supportRoutes);
