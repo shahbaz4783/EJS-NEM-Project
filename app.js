92a4384cb99642a81996f27702de146ab25be076
shahbaz4783 <shahbaz4783@gmail.com>
2023-12-23 22:10:17 +0530

    fix small issues

--- app.js
+++ app.js
@@ -26,6 +26,18 @@ app.use(
 	})
 );
 
+app.use((req, res, next) => {
+	if (!req.session.user) {
+		return next();
+	}
+	User.findById(req.session.user._id)
+		.then((user) => {
+			req.user = user;
+			next();
+		})
+		.catch((err) => console.log(err));
+});
+
 app.use(routes);
 app.use(error);
 
